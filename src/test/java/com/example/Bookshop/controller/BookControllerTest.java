b1a0d1392955d6fa5d2b9e7b4f0866c4f8de2177
DooM3P <ludovic.lecoq@gmail.com>
2023-01-10 17:18:10 +0100

    temporary add test

--- src/test/java/com/example/Bookshop/controller/BookControllerTest.java
+++ src/test/java/com/example/Bookshop/controller/BookControllerTest.java
@@ -6,12 +6,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
@@ -51,5 +53,27 @@ public class BookControllerTest {
 
         //then
         assertTrue(bookController.getBooks().getStatusCode()==HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Test
+    public void deleteBooksTest() throws Exception {
+        //given
+        Book book = new Book();
+        book.setId(2L);
+        book.setTitle("Hhhh");
+        book.setAuthor("Gilles H");
+        book.setPrice(30);
+
+        //when
+
+        Mockito.when(bookService.saveBook(book)).thenReturn(book);
+        bookController.saveBook(book);
+        bookController.deleteAll();
+
+        ResponseEntity<Book> response = bookController.getBookById(2L);
+
+        //then
+        assertThat(response.getBody()).isNull();
+
     }
 }
